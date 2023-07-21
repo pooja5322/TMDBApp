f885f678fc6961e4426f7b17452629f59ac3ab63
Pooja Shelke <poojashelke43@gmail.com>
2023-07-21 19:04:56 +0530

    updated code

--- app/src/main/java/com/example/demoapp/common/Constants.kt
+++ app/src/main/java/com/example/demoapp/common/Constants.kt
@@ -2,5 +2,5 @@ package com.example.myapplication.common
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/"
-    const val API_KEY = "2ed5dd1c48f6cf11ad1d7e0fb1703447"
+    const val API_KEY = "{API_KEY}"
 }
